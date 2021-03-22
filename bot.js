3b1c9f7b66b0f04512a3fb893451e294f675c12c
cafecat6503 <kaito.k.0725@gmail.com>
2021-03-22 20:40:40 +0900

    とりあえず稼働するようになった

--- bot.js
+++ bot.js
@@ -4,49 +4,64 @@ const env = process.env
 const Discord = require('discord.js')
 const discordClient = new Discord.Client()
 
-const date = new Date()
+const date = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' })
+
+const axios = require('axios')
+
+const fs = require('fs')
 
 //ここからbotの本体部分
 
-const adminId = '373846007219486721'
+//権限周り
+const ownerId = '369876595051069440'
+const adminRoles = []
 
-discordClient.on('ready', () => { //開始時のログ出力
-    console.log(`${date.toLocaleString()} : Bot is ready`)
+const mankoChannelId = ''
+
+const commandPrefix = '!'
+const commandPattern = /[^\s]+/g
+const useridPattern = /[0-9]+/
+
+discordClient.on('ready', () => {
+    console.log(`${date} : Bot is ready`)
 })
-
-/* discordClient.on('ready', (msg) => {
-    if (msg.author.bot) {
-        return
-    }
-}
-) */
 
 discordClient.on('message', msg => {
 
-    if (msg.content === 'Hello') {
-        const messageSentence = "world!"
-        const messageAutherByUsername = msg.author.username
-        msg.reply(messageSentence)
-        console.log(`${date.toLocaleString()} : ${meg.content} was sent to ${msg.author.username}.`)
-    }
+    // 画像のロギング
+    if (msg.attachments.first | !msg.author === ownerId) {
+        const attachmentFileUrls = msg.attachments.map(attachment => attachment.url)
+        const attachmentFileNames = msg.attachments.map(attachment => attachment.name)
+        for (let index = 0; index < attachmentFileNames.length; index++) {
+            let snowflake = Discord.SnowflakeUtil.generate()
+            let fileName = './images/' + `${snowflake}` + '-' + `${attachmentFileNames[index]}`
 
-    if (msg.channel.id === '') {
-        console.log(`${date.toLocaleString()} : ${msg.author.username} #${msg.channel.name} ${msg.content}`)
-    }
-
-
-    if (msg.content === "!shutdown") { //bot停止用コマンド
-        if (msg.author.id === adminId) {
-            console.log(`${date.toLocaleString()} : Bot will be stop.`)
-            process.exit()
-        } else {
-            msg.reply('権限がありません。このコマンドは管理者用です。')
-            console.log(`${date.toLocaleString()} : Unauthorized command: [${msg.content}] have been executed by ${msg.author.username}.`)
+            axios.get(attachmentFileUrls[index], { responseType: 'arraybuffer' })
+                .then(
+                    response => {
+                        fs.writeFileSync(fileName, Buffer.from(response.data), 'binary')
+                        console.log(`${date} : ${fileName} was saved.`)
+                    }
+                )
         }
     }
+
+
+    if (msg.content.startsWith(commandPrefix)) {
+        const userCommand = msg.content.match(commandPattern)
+
+        if (userCommand[0] == '!卒業' | useridPattern.test(userCommand[1])) {
+            msg.channel.send('コマンドが未定義です。')
+        }
+
+        if (userCommand[0] == '!manko' | userCommand[0] == '!manco') {
+            msg.channel.send('oh!manko!')
+        }
+
+    }
+
 }
+
 )
 
 discordClient.login(env.DISCORD_BOT_TOKEN)
-
-//終了
